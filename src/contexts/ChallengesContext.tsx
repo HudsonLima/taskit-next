cff2a9ea3b2eb34c4675ac44f4c7be4e9d9cc7ae
Hudson Lima <hudsonlima.ti@gmail.com>
2021-02-28 19:44:05 -0300

    Create completeChallenge func

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -17,7 +17,8 @@ interface ChallengesContextData {
     levelUp: () => void;
     activeChallenge: Challenge;
     startNewChallenge: () => void;
-    resetChallenge: () => void;    
+    resetChallenge: () => void; 
+    completeChallenge: () => void;   
 }
 
 interface ChallengesProviderProps {
@@ -50,6 +51,25 @@ export function ChallengesProvider({ children  } : ChallengesProviderProps) {
         setActiveChallenge(null);
     }
 
+    function completeChallenge() {
+        if(!activeChallenge)
+        {
+            return;
+        }
+        const { amount } = activeChallenge
+        let finalExperience = currentExperience + amount;
+
+        if(finalExperience >= experienceToNextLevel)
+        {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
+
     return (
         <ChallengesContext.Provider 
         value={{ 
@@ -60,7 +80,8 @@ export function ChallengesProvider({ children  } : ChallengesProviderProps) {
             startNewChallenge ,
             activeChallenge,
             resetChallenge,
-            experienceToNextLevel
+            experienceToNextLevel,
+            completeChallenge
             }}>
             { children  }
         </ChallengesContext.Provider>
